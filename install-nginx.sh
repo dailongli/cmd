239b7efc39558f65e00567a79ff2425d92b5cf92
Dailong Li <dailongli@hotmail.com>
2021-01-17 20:55:16 -0800

    download nginx and uncompress

--- install-nginx.sh
+++ install-nginx.sh
@@ -3,4 +3,4 @@ apt-get update
 apt-get -y upgrade
 
 # download nginx and uncompress
-curl http://nginx.org/download/nginx-1.18.0.tar.gz -o nginx-1.18.0.tar.gz | tar -xz
+curl http://nginx.org/download/nginx-1.18.0.tar.gz | tar -xz
